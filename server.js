16ada71089c6eae98e7ed10270efcdf70059bd6e
Baraa Sunjakdar <sunjakdarbaraa22@gmail.com>
2025-03-06 03:58:00 +0100

    Hosting 1.1

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const multer = require('multer');
 const sharp = require('sharp');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 // Statisches Verzeichnis, in dem die Webseite (HTML, CSS) liegt
 app.use(express.static(path.join(__dirname, 'public')));
